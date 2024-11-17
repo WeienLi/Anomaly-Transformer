d103034c4020ec7748567505728cb7ca9aae9a68
WeienLi <weien.li@mail.mcgill.ca>
2024-11-17 18:26:35 -0500

    Added some comment for models

--- model/attn.py
+++ model/attn.py
@@ -8,7 +8,17 @@ import os
 
 
 class TriangularCausalMask():
+    """Regular Triangular Masking
+    """
     def __init__(self, B, L, device="cpu"):
+        """Constructor
+
+        Args:
+            B (int): Batch size
+            L (int): sequence_length
+            device (str, optional): _description_. Defaults to "cpu".
+        """
+        # B1LL because B is batch LL create a square matrix 1 helps for broadcasting used in masked_fill later on/
         mask_shape = [B, 1, L, L]
         with torch.no_grad():
             self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
@@ -19,40 +29,56 @@ class TriangularCausalMask():
 
 
 class AnomalyAttention(nn.Module):
+    """Anamoly Attention class describe in the paper
+    """
     def __init__(self, win_size, mask_flag=True, scale=None, attention_dropout=0.0, output_attention=False):
+        """Constructor
+        """
         super(AnomalyAttention, self).__init__()
         self.scale = scale
-        self.mask_flag = mask_flag
-        self.output_attention = output_attention
+        self.mask_flag = mask_flag #mask flag
+        self.output_attention = output_attention # output attention flag
         self.dropout = nn.Dropout(attention_dropout)
-        window_size = win_size
-        self.distances = torch.zeros((window_size, window_size)).cuda()
+        window_size = win_size #token size
+        self.distances = torch.zeros((window_size, window_size)).cuda() #prior distance calculator
         for i in range(window_size):
             for j in range(window_size):
-                self.distances[i][j] = abs(i - j)
+                self.distances[i][j] = abs(i - j) # how far away i and j are
 
     def forward(self, queries, keys, values, sigma, attn_mask):
-        B, L, H, E = queries.shape
-        _, S, _, D = values.shape
-        scale = self.scale or 1. / sqrt(E)
+        """How this class deal with the input
 
-        scores = torch.einsum("blhe,bshe->bhls", queries, keys)
-        if self.mask_flag:
+        Args:
+            queries (Tensor): Queries of Transformer
+            keys (Tensor): Key of Transformer
+            values (Tensor): Value of Transformer
+            sigma (Tensor): Prior Association Sigma
+            attn_mask (Tensor): Attention mask
+
+        Returns:
+            tuple: Reconstruction, series prior and sigma
+        """
+        B, L, H, E = queries.shape # Batch, Seq_length, Head, Embedding
+        _, S, _, D = values.shape # Batch same as quries, sequence length of values, Head same as queries, Embedding of value as D
+        scale = self.scale or 1. / sqrt(E) #E here is the embedding size
+
+        scores = torch.einsum("blhe,bshe->bhls", queries, keys) #dot product between the embedding dimension of queries and keys Matmul
+        if self.mask_flag: #If require an attention mask create a traingular causal mask
             if attn_mask is None:
                 attn_mask = TriangularCausalMask(B, L, device=queries.device)
-            scores.masked_fill_(attn_mask.mask, -np.inf)
-        attn = scale * scores
+            scores.masked_fill_(attn_mask.mask, -np.inf) #fill masking position with negative inf for softmax since softmax of negative inf become 0.
+        attn = scale * scores #apply the scale to the scores (query * value)
 
-        sigma = sigma.transpose(1, 2)  # B L H ->  B H L
+        sigma = sigma.transpose(1, 2)  # B L H ->  B H L #Batch Sequence length head to batch head seq_length
         window_size = attn.shape[-1]
-        sigma = torch.sigmoid(sigma * 5) + 1e-5
-        sigma = torch.pow(3, sigma) - 1
-        sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size)  # B H L L
-        prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(sigma.shape[0], sigma.shape[1], 1, 1).cuda()
-        prior = 1.0 / (math.sqrt(2 * math.pi) * sigma) * torch.exp(-prior ** 2 / 2 / (sigma ** 2))
+        sigma = torch.sigmoid(sigma * 5) + 1e-5 #normalize sigma and adds numeric stability using 1e-5
+        sigma = torch.pow(3, sigma) - 1 #make sigma larger make sigma start from 0
+        sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size)  # B H L L #add new last dimension and repeat for window_size stamp for the last sequence length.
+        prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(sigma.shape[0], sigma.shape[1], 1, 1).cuda() #broadcast the distances
+        prior = 1.0 / (math.sqrt(2 * math.pi) * sigma) * torch.exp(-prior ** 2 / 2 / (sigma ** 2)) #prior calculation from the paper
 
-        series = self.dropout(torch.softmax(attn, dim=-1))
-        V = torch.einsum("bhls,bshd->blhd", series, values)
+        series = self.dropout(torch.softmax(attn, dim=-1)) #Transformer final softmax
+        V = torch.einsum("bhls,bshd->blhd", series, values) #KQ matmul V
 
         if self.output_attention:
             return (V.contiguous(), series, prior, sigma)
@@ -61,8 +87,19 @@ class AnomalyAttention(nn.Module):
 
 
 class AttentionLayer(nn.Module):
+    """The full antteion Layer
+    """
     def __init__(self, attention, d_model, n_heads, d_keys=None,
                  d_values=None):
+        """Constructor
+
+        Args:
+            attention (nn layers): some attention layer we want to use (AnomalyAttention)
+            d_model (int): dimension of the model
+            n_heads (int): heads of transformer
+            d_keys (int, optional): dimension of keys or else d_model//n_heads . Defaults to None.
+            d_values (int, optional): dimension of values or else d_model//n_heads. Defaults to None.
+        """
         super(AttentionLayer, self).__init__()
 
         d_keys = d_keys or (d_model // n_heads)
@@ -82,14 +119,25 @@ class AttentionLayer(nn.Module):
         self.n_heads = n_heads
 
     def forward(self, queries, keys, values, attn_mask):
+        """Regular Transformer Forward
+
+        Args:
+            queries (Tensor): Queries of Transformer
+            keys (Tensor): Key of Transformer
+            values (Tensor): Value of Transformer
+            attn_mask (Tensor): Attention mask
+
+        Returns:
+            tuple: Reconstruction, series prior and sigma
+        """
         B, L, _ = queries.shape
         _, S, _ = keys.shape
-        H = self.n_heads
+        H = self.n_heads #how many head to split the embedding
         x = queries
-        queries = self.query_projection(queries).view(B, L, H, -1)
-        keys = self.key_projection(keys).view(B, S, H, -1)
-        values = self.value_projection(values).view(B, S, H, -1)
-        sigma = self.sigma_projection(x).view(B, L, H)
+        queries = self.query_projection(queries).view(B, L, H, -1) #Linear layer x*Wq
+        keys = self.key_projection(keys).view(B, S, H, -1) #Linear layer x*Wk
+        values = self.value_projection(values).view(B, S, H, -1) #Linear layer x*Wv
+        sigma = self.sigma_projection(x).view(B, L, H) #Sigma is obtained from query passing through linear
 
         out, series, prior, sigma = self.inner_attention(
             queries,
@@ -97,7 +145,7 @@ class AttentionLayer(nn.Module):
             values,
             sigma,
             attn_mask
-        )
-        out = out.view(B, L, -1)
+        ) #Like regular attention after passing through the linear layer pass through the attention
+        out = out.view(B, L, -1) #take out the head dimension to get back to embedding dimenion
 
         return self.out_projection(out), series, prior, sigma
--- model/embed.py
+++ model/embed.py
@@ -5,34 +5,58 @@ from torch.nn.utils import weight_norm
 import math
 
 
+
 class PositionalEmbedding(nn.Module):
+    """Sinusoidal Positional Encoding"""
     def __init__(self, d_model, max_len=5000):
+        """Sinusoidal Positional Encoding Constructor
+
+        Args:
+            d_model (int): The number of features/embedding dimension
+            max_len (int, optional): _description_. Defaults to 5000.
+        """
         super(PositionalEmbedding, self).__init__()
         # Compute the positional encodings once in log space.
+        
+        #Create a pe tensor of shape max_len, d_model
         pe = torch.zeros(max_len, d_model).float()
         pe.require_grad = False
 
+        #Generates a tensor of position indices from 0 to max_len-1 (index)
         position = torch.arange(0, max_len).float().unsqueeze(1)
+        # Calculates the division terms for the sinusoidal functions.
         div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
 
+        #Even index sin Odd index cosine
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
 
+        #make pe tensor of shape 1,max_len,d_model
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
+        # take out (1, sequence_length, d_model)
         return self.pe[:, :x.size(1)]
 
 
 class TokenEmbedding(nn.Module):
+    """Token Embedding Embeddes each token (Embedder of Transformer)"""
     def __init__(self, c_in, d_model):
+        """Constructor of Token Embedder of Transformer
+
+        Args:
+            c_in (_type_): the number of feature we would like to pass in for embedding
+            d_model (_type_): dimension that we would like the embedder to output
+        """
         super(TokenEmbedding, self).__init__()
         padding = 1 if torch.__version__ >= '1.5.0' else 2
+        # Circular padding add the last sequence to the first
         self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,
                                    kernel_size=3, padding=padding, padding_mode='circular', bias=False)
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
+                # Intialize the conv1d weight with kaiming he intialization method
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
 
     def forward(self, x):
@@ -41,7 +65,15 @@ class TokenEmbedding(nn.Module):
 
 
 class DataEmbedding(nn.Module):
+    """Combined class of Embedder and Positional Encoding"""
     def __init__(self, c_in, d_model, dropout=0.0):
+        """Constructor
+
+        Args:
+            c_in (_type_): input dimension of feature space within data 
+            d_model (_type_): dimension that we would like the embedder to output
+            dropout (float, optional): Dropout rate after embedding and positional encoding. Defaults to 0.0.
+        """
         super(DataEmbedding, self).__init__()
 
         self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
@@ -50,5 +82,6 @@ class DataEmbedding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
+        # put everything together classic transformer techniques
         x = self.value_embedding(x) + self.position_embedding(x)
         return self.dropout(x)
